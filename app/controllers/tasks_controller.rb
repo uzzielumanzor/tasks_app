7d5ef70bc041c2928a67cb3621b62ca59f47e748
Uzziel Umanzor <uzziel_umanzor@hotmail.com>
2012-05-20 15:57:05 -0600

    heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,13 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
+    redirect_to tasks_path 
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def show
